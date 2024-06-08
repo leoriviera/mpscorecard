1e9d5c8dedcca3e18ccd7f64cf42a3b4a1951d50
Curtis Turk <curtis.c.turk@outlook.com>
2024-06-08 18:06:27 +0100

    Initial titles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,6 +26,7 @@ export default function Home() {
 
   return (
     <main
+<<<<<<< HEAD
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
@@ -127,6 +128,12 @@ export default function Home() {
           </p>
         </a>
       </div>
+=======
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <h1>How did your MP vote on the issues that mattered most this year?</h1>
+      <h2>Enter your postcode to get their 2022 Report Card.</h2>
+>>>>>>> 6f89921 (Initial titles)
     </main>
   );
 }
