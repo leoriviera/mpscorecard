75a2baf09f694a24acc2cc18e6da65de1577c118
Curtis Turk <curtis.c.turk@outlook.com>
2024-06-08 19:10:18 +0100

    refactored index with ref

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
-import { useEffect } from "react";
-import PostcodeInput from "./PostcodeInput";
+import { useEffect, useRef } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +23,8 @@ const fetchConstituency = async (postcode: string) => {
 export default function Home() {
   useEffect(() => {}, []);
 
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
@@ -31,7 +32,27 @@ export default function Home() {
       <h1>How did your MP vote on the issues that mattered most this year?</h1>
       <h2>Enter your postcode to get their 2022 Report Card.</h2>
       <div>Your postcode</div>
-      <PostcodeInput />
+
+      <div>
+        <h1>Form Page</h1>
+        <form>
+          <input required ref={inputRef} name="Postcode" placeholder="" />
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+
+              if (inputRef.current) {
+                const { value } = inputRef.current;
+
+                console.log(value);
+              }
+            }}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </main>
   );
 }
