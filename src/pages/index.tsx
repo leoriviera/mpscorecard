109e1018193c9c261c400988ed2690c87fc34416
Leo Riviera <leo.riviera@voxa.dev>
2024-06-09 10:54:45 +0100

    designed homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import { Inter } from "next/font/google";
-import { useEffect, useRef } from "react";
-import { Scorecard } from "./constituency/[slug]";
 import kebabCase from "lodash.kebabcase";
 import { useRouter } from "next/router";
+import { Field, Form, Formik } from "formik";
+import * as yup from "yup";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const clsx = (...classnames: (string | false | undefined)[]) =>
+  classnames.filter(Boolean).join(" ");
 
 // TODO - add error state to form field
 // empty postcode, invalid postcode
@@ -14,65 +17,95 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   const { push } = useRouter();
 
-  const redirectToConstituency = async (postcode: string) => {
-    const find = `https://api.postcodes.io/postcodes/${encodeURIComponent(
-      postcode
-    )}`;
-
-    const response = await fetch(find);
-
-    const {
-      result: { parliamentary_constituency },
-    } = (await response.json()) as {
-      result: {
-        parliamentary_constituency: string;
-      };
-    };
-
-    await push(`/constituency/${kebabCase(parliamentary_constituency)}`);
-  };
-
-  const inputRef = useRef<HTMLInputElement | null>(null);
-
   return (
     <main
-      className={`flex-col items-center justify-center min-h-screen mx-16 sm:px-6 lg:px-8 ${inter.className}`}>
-      <div className={`max-w-5xl mx-auto> pt-20`}>
-        <h1 className={`text-6xl font-bold mb-4 text-gray-800`}>
-          How did your MP vote on the issues that mattered most this year?
+      className={clsx(
+        "flex-col min-h-screen min-w-full px-6 lg:px-16 content-center bg-gradient-to-br from-yellow-100 via-blue-300 to-red-200",
+        inter.className
+      )}>
+      <div className='mx-auto'>
+        <h1 className='max-w-2xl text-3xl md:text-6xl font-extrabold mb-4 text-gray-800 uppercase'>
+          How did your MP vote over the last Parliament?
         </h1>
-        <h2 className={`mb-8 text-lg text-gray-600`}>
-          Enter your postcode to get their 2024 Report Card.
+        <h2 className='max-w-xl mb-8 text-xl md:text-4xl text-gray-600 uppercase font-bold'>
+          Enter your postcode to get their scorecard.
         </h2>
       </div>
 
       <div>
-        <div className='text-lg text-gray-700'>Your postcode</div>
-        <form>
-          <input
-            required
-            ref={inputRef}
-            name='Postcode'
-            placeholder=''
-            className='mb-4 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 pr-5'
-          />
-          <button
-            type='submit'
-            onClick={async (e) => {
-              e.preventDefault();
+        <Formik
+          initialValues={{
+            postcode: "",
+          }}
+          validationSchema={yup.object().shape({
+            postcode: yup
+              .string()
+              .trim()
+              .required("Please enter a postcode.")
+              .matches(/^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$/i, {
+                message: "That postcode doesn't look valid!",
+              })
+              .notOneOf(
+                ["SANTA1", "SAN TA1"],
+                "Ho ho ho! Santa doesn't have an MP! 🎅🏼"
+              ),
+          })}
+          onSubmit={async (values, { setErrors }) => {
+            const { postcode } = values;
 
-              if (inputRef.current) {
-                const { value } = inputRef.current;
+            const find = `https://api.postcodes.io/postcodes/${encodeURIComponent(
+              postcode
+            )}`;
 
-                await redirectToConstituency(value);
-              }
-            }}
-            className='px-6 py-3 bg-red-600 text-white font-bold rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500'>
-            Submit
-          </button>
-        </form>
+            const response = await fetch(find);
+
+            try {
+              const {
+                result: { parliamentary_constituency },
+              } = (await response.json()) as {
+                result: {
+                  parliamentary_constituency: string;
+                };
+              };
+
+              await push(
+                `/constituency/${kebabCase(parliamentary_constituency)}`
+              );
+            } catch {
+              setErrors({
+                postcode:
+                  "We couldn't find that postcode. Please try another one!",
+              });
+            }
+          }}>
+          {({ touched, errors, submitCount }) => (
+            <Form>
+              <label
+                htmlFor='postcode'
+                className='text-md md:text-2xl text-gray-700 uppercase font-light'>
+                Your postcode
+              </label>
+              <div className='flex mt-2'>
+                <Field
+                  required
+                  id='postcode'
+                  name='postcode'
+                  placeholder='SW1A 2AA'
+                  className='px-2 md:px-4 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+                />
+                <button
+                  type='submit'
+                  className='text-sm md:text-base h-full px-3 md:px-4 py-3 bg-red-600 text-white font-bold rounded-r-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500'>
+                  Submit
+                </button>
+              </div>
+              {touched.postcode && errors.postcode && submitCount >= 1 && (
+                <p className='mt-2 text-sm text-red-600'>{errors.postcode}</p>
+              )}
+            </Form>
+          )}
+        </Formik>
       </div>
-      <Scorecard></Scorecard>
     </main>
   );
 }
