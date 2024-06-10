359d7a1349762a490e789c18b11a6c2c9971c488
Leo Riviera <leo.riviera@voxa.dev>
2024-06-10 12:19:34 +0100

    initial scorecard

--- src/pages/constituency/[slug].tsx
+++ src/pages/constituency/[slug].tsx
@@ -1,64 +1,84 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Image from "next/image";
 import kebabCase from "lodash.kebabcase";
+
 import constituencies from "../data/constituencies.json";
+import parties from "../data/parties.json";
+import { clsx } from "../utils";
 
 interface Vote {
   description: string;
-  grade: "A" | "F" | "-";
+  grade: "A" | "F";
 }
 
-interface MpScorecard {
+interface MPScorecard {
   name: string;
-  party: string;
+  party: (typeof parties)[keyof typeof parties];
   constituency: string;
   votes: Vote[];
+  absent: string[];
+  none: string[];
   thumbnail: string;
 }
 
-export const Scorecard: NextPage = () => {
-  let mpscore: MpScorecard = {
-    name: "Keir Starmer",
-    party: "Labour",
-    constituency: "Holborn and St Pancras",
-    votes: [
-      {
-        description: "voted against puppy murder",
-        grade: "A",
-      },
-    ],
-    thumbnail:
-      "https://upload.wikimedia.org/wikipedia/commons/1/1f/Official_portrait_of_Keir_Starmer_%28crop%29.jpg",
-  };
+export const Scorecard: NextPage<{ score: MPScorecard }> = ({ score }) => {
   // Render scorecard page
+
   return (
-    <div className='max-w-xl mx-auto bg-neutral-100 rounded-lg shadow-md p-6'>
-      <div className='flex items-center mb-4'>
-        <Image
-          src={mpscore.thumbnail}
-          width={50}
-          height={100}
-          alt={mpscore.name + " image"}
-          className='rounded-full mr-4'
-        />
-        <h1 className='text-2xl font-bold'>Your MP&apos;s 2024 Report Card</h1>
+    // ea1d0e
+    <div
+      className={clsx(
+        "min-h-screen min-w-full content-center bg-gradient-to-br from-neutral-200 px-6 lg:px-16",
+        score.party.colour
+      )}>
+      <div className='flex flex-col p-6 lg:p-10 bg-neutral-200 max-w-3xl rounded-md border-4 border-neutral-200'>
+        <div className='flex items-center mb-4'>
+          <Image
+            src={score.thumbnail}
+            width={160}
+            height={160}
+            alt={score.name}
+            className='object-cover object-top w-40 h-40 rounded-full mr-4'
+          />
+          <div>
+            <h1 className='text-5xl font-bold'>Scorecard for</h1>
+            <p className='font-extrabold text-3xl'>{score.name}</p>
+            <p className='font-bold text-xl'>{score.party.name}</p>
+            <p className='font-semibold text-xl'>
+              Member of Parliament for {score.constituency}
+            </p>
+          </div>
+        </div>
+
+        <div className='space-y-2'>
+          <h2 className='text-3xl font-medium'>
+            During the last Parliament, your MP voted
+          </h2>
+
+          {score.votes.map((vote, index) => (
+            <div
+              key={index}
+              className='flex flex-row justify-between text-2xl font-extrabold'>
+              <span>{vote.description}</span>
+              <strong className='text-green-600 font-bold'>{vote.grade}</strong>
+            </div>
+          ))}
+        </div>
+        <div className='mt-4 space-y-2'>
+          <p className='text-md font-light font-gray-200 italic'>
+            Your MP was absent for votes on outlawing ding-dong-ditch, closing
+            every Tesco, removing the cap on bankers&apos; bonuses and removing
+            Scunthorpe from the United Kingdom. An MP may have been absent for
+            several reasons, including illness or handling constituency
+            business.
+          </p>
+          <p className='text-md font-light font-gray-200 italic'>
+            Your MP couldn&apos;t vote on potato theft, children in mines or
+            supermarket profiteering, as they weren&apos;t an MP when the vote
+            was held.
+          </p>
+        </div>
       </div>
-      <div className='mb-4'>
-        <div className=''>Your MP: {mpscore.name}</div>
-        <div className=''>Party: {mpscore.party}</div>
-        <div className=''>Constituency: {mpscore.constituency}</div>
-      </div>
-      <h2 className='text-xl font-bold mb-2'>
-        How your MP voted on these key issues this year:
-      </h2>
-      <ul>
-        {mpscore.votes.map((vote, index) => (
-          <li key={index} className='flex items-center mb-2'>
-            <span className=' mr-2'>{vote.description}:</span>
-            <strong className='text-green-600 font-bold'>{vote.grade}</strong>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
@@ -67,7 +87,9 @@ export const Scorecard: NextPage = () => {
 export const getStaticPaths: GetStaticPaths = async () => {
   // Generate the paths we want to pre-render based on posts
   const paths = Object.keys(constituencies).map((c) => ({
-    params: { slug: kebabCase(c) },
+    params: {
+      slug: kebabCase(c),
+    },
   }));
 
   // We'll pre-render only these paths at build time.
@@ -94,17 +116,37 @@ export const getStaticProps: GetStaticProps<
 
   const { slug } = params;
 
-  const constituencyInfo = constituencies[slug];
+  // const constituencyInfo = constituencies[slug];
 
-  // Call an external API endpoint to get posts
-  const res = await fetch("https://.../posts");
-  const posts = await res.json();
+  const party = parties["labour"];
 
-  // By returning { props: { posts } }, the Blog component
+  // By returning { props: { posts } }, the Scorecard component
   // will receive `posts` as a prop at build time
   return {
     props: {
-      posts,
+      score: {
+        name: "Keir Starmer",
+        party,
+        constituency: "Holborn and St Pancras",
+        votes: [
+          {
+            description: "against puppy murder",
+            grade: "A",
+          },
+          {
+            description: "for world peace",
+            grade: "A",
+          },
+          {
+            description: "for fracking",
+            grade: "F",
+          },
+        ],
+        absent: ["conversion therapy", "breaking mafioso knees"],
+        none: ["playing music on public buses"],
+        thumbnail:
+          "https://upload.wikimedia.org/wikipedia/commons/1/1f/Official_portrait_of_Keir_Starmer_%28crop%29.jpg",
+      },
     },
   };
 };
