6285af1b2682687e3e77352449450629b1725007
Leo Riviera <leo.riviera@voxa.dev>
2024-06-15 14:08:25 +0100

    add votes to page

--- src/pages/constituency/[slug].tsx
+++ src/pages/constituency/[slug].tsx
@@ -6,114 +6,106 @@ import {
   XCircleIcon,
   QuestionMarkCircleIcon,
 } from "@heroicons/react/24/solid";
-import { clsx } from "../utils";
+import { clsx } from "../../utils";
 
 import constituencies from "../data/constituencies.json";
+import divisions from "../data/divisions.json";
 import parties from "../data/parties.json";
 
-interface Vote {
+type Vote = {
+  id: string;
   description: string;
-  grade: "aye" | "noe";
-}
+  grade: "tick" | "x" | "unknown";
+};
 
-interface MPScorecard {
-  name: string;
+type MPScorecard = {
+  constituency: (typeof constituencies)[keyof typeof constituencies]["constituency"];
+  member: Omit<
+    (typeof constituencies)[keyof typeof constituencies]["member"],
+    "divisions"
+  >;
   party: (typeof parties)[keyof typeof parties];
-  constituency: string;
   votes: Vote[];
-  absent: string[];
-  none: string[];
-  thumbnail: string;
-}
+};
 
-export const Scorecard: NextPage<{ score: MPScorecard }> = ({ score }) => {
+export const Scorecard: NextPage<{ data: MPScorecard }> = ({ data }) => {
   return (
     <div
       className={clsx(
         "min-h-screen min-w-full content-center bg-gradient-to-br from-neutral-200 p-6 lg:px-16",
-        score.party.gradient
+        data.party.gradient
       )}>
-      <div className='flex flex-col p-4 md:p-6 lg:p-10 bg-neutral-100 max-w-3xl rounded-md border-2 border-black'>
+      <div className='flex flex-col max-w-3xl p-4 border-2 border-black rounded-md md:p-6 lg:p-10 bg-neutral-100'>
         {/* Scorecard header */}
-        <div className='flex flex-col md:flex-row items-center'>
+        <div className='flex flex-col items-center md:flex-row'>
           <Image
-            src={score.thumbnail}
+            src={data.member.thumbnail}
             width={160}
             height={160}
-            alt={score.name}
-            className='object-cover object-top w-40 h-40 rounded-full border-black border-2 shrink-0'
+            alt={data.member.name}
+            className='object-cover object-top w-40 h-40 border-2 border-black rounded-full shrink-0'
           />
-          <div className='pt-4 md:pt-0 md:pl-10 w-full text-center md:text-left'>
-            <p className='text-sm md:text-md font-bold uppercase'>Scorecard</p>
-            <p className='font-extrabold text-2xl md:text-4xl'>{score.name}</p>
+          <div className='w-full pt-4 text-center md:pt-0 md:pl-10 md:text-left'>
+            <p className='text-sm font-bold uppercase md:text-md'>Scorecard</p>
+            <p className='text-2xl font-extrabold md:text-4xl'>
+              {data.member.name}
+            </p>
             <p
               className={clsx(
                 "font-bold text-xl md:text-2xl",
-                score.party.colour
+                data.party.colour
               )}>
-              {score.party.name}
+              {data.party.name}
             </p>
             <p className='font-semibold md:text-xl'>
-              MP for {score.constituency}
+              MP for {data.constituency.name}
             </p>
           </div>
         </div>
 
-        <div className='border border-black -mx-4 md:-mx-6 lg:-mx-10 my-4'></div>
+        <div className='my-4 -mx-4 border border-black md:-mx-6 lg:-mx-10'></div>
 
         {/* Votes */}
         <div className='space-y-2'>
-          <h2 className='text-xl md:text-2xl font-bold'>
+          <h2 className='text-xl font-bold md:text-2xl'>
             During the last Parliament, your MP...
           </h2>
 
-          {score.votes.map((vote, index) => (
+          {data.votes.map((vote, index) => (
             <div
               key={index}
-              className='flex flex-row justify-between items-center space-x-2'>
-              <p className='font-medium text-xl md:text-2xl'>
-                {vote.description}
-              </p>
+              className='flex flex-row items-center justify-between space-x-2'>
+              <p className='text-xl md:text-2xl'>{vote.description}</p>
               {
                 {
-                  aye: (
-                    <CheckCircleIcon className='h-12 w-12 md:h-16 md:w-16 text-green-500 shrink-0' />
+                  tick: (
+                    <CheckCircleIcon className='w-12 h-12 text-green-500 md:h-16 md:w-16 shrink-0' />
                   ),
-                  noe: (
-                    <XCircleIcon className='h-12 w-12 md:h-16 md:w-16 text-red-500 shrink-0' />
+                  x: (
+                    <XCircleIcon className='w-12 h-12 text-red-500 md:h-16 md:w-16 shrink-0' />
+                  ),
+                  unknown: (
+                    <QuestionMarkCircleIcon className='w-12 h-12 text-yellow-500 md:h-16 md:w-16 shrink-0' />
                   ),
                 }[vote.grade]
               }
             </div>
           ))}
 
-          {score.absent.length ? (
-            <>
-              {score.absent.map((vote, index) => (
-                <div
-                  key={index}
-                  className='flex flex-row justify-between items-center space-x-2'>
-                  <span className='text-xl md:text-2xl font-medium'>
-                    {vote}
-                  </span>
-                  <QuestionMarkCircleIcon className='h-12 w-12 md:h-16 md:w-16 text-yellow-500 shrink-0' />
-                </div>
-              ))}
-
-              <div className='md:pt-2'>
-                <p className='text-sm md:text-md font-light font-gray-200'>
-                  The{" "}
-                  <span className='text-yellow-700 font-bold'>
-                    yellow question mark icon
-                  </span>{" "}
-                  means no vote was recorded for your MP. An MP may have no vote
-                  recorded for several reasons, including if they weren&apos;t
-                  an MP at the time, if they abstained from a vote, were ill or
-                  were carrying out constituency or ministerial business.
-                </p>
-              </div>
-            </>
-          ) : null}
+          {data.votes.filter((v) => v.grade === "unknown").length > 0 && (
+            <div className='md:pt-2'>
+              <p className='text-sm font-light md:text-md font-gray-200'>
+                The{" "}
+                <span className='font-bold text-yellow-700'>
+                  yellow question mark icon
+                </span>{" "}
+                means no vote was recorded for your MP. An MP may have no vote
+                recorded for several reasons, including if they weren&apos;t an
+                MP at the time, if they abstained from a vote, were ill or were
+                carrying out constituency or ministerial business.
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
@@ -144,47 +136,43 @@ export const getStaticProps: GetStaticProps<
     slug: keyof typeof constituencies;
   }
 > = async ({ params }) => {
-  if (!params) {
-    // TODO - redirect if no params
-    return {
-      props: {},
-    };
-  }
+  // Params should always have slug, as
+  // page is statically generated based on paths
+  const { slug } = params!;
 
-  const { slug } = params;
+  const {
+    constituency,
+    member: { divisions: memberDivisions, ...member },
+  } = constituencies[slug];
 
-  // const constituencyInfo = constituencies[slug];
+  const party = parties[member.party as keyof typeof parties];
 
-  const party = parties["labour"];
+  const votes = Object.entries(memberDivisions).map(
+    ([id, { vote, teller }]) => {
+      const { positive } = divisions[id as keyof typeof divisions];
+      const { description } =
+        divisions[id as keyof typeof divisions][
+          vote as "aye" | "noe" | "notRecorded"
+        ];
+
+      return {
+        id,
+        description: `${description} ${teller ? "(as a teller)" : ""}`,
+        grade:
+          vote === "notRecorded" ? "unknown" : positive === vote ? "tick" : "x",
+      };
+    }
+  );
 
   // By returning { props: { posts } }, the Scorecard component
   // will receive `posts` as a prop at build time
   return {
     props: {
-      score: {
-        name: "Keir Starmer",
+      data: {
+        constituency,
         party,
-        constituency: "Holborn and St Pancras",
-        votes: [
-          {
-            description: "voted against puppy murder",
-            grade: "aye",
-          },
-          {
-            description: "voted for a ban on fracking",
-            grade: "noe",
-          },
-          {
-            description:
-              "voted against cracking down on puppy smuggling and dog thefts",
-            grade: "noe",
-          },
-        ],
-        absent: [
-          "did not vote on requiring water companies to reduce sewage discharge into British rivers and seas",
-        ],
-        thumbnail:
-          "https://upload.wikimedia.org/wikipedia/commons/1/1f/Official_portrait_of_Keir_Starmer_%28crop%29.jpg",
+        member,
+        votes,
       },
     },
   };
