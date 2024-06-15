e958e6418c3554db82a73ebb8ecb3b21e38e209c
Leo Riviera <leo.riviera@voxa.dev>
2024-06-15 11:24:39 +0100

    fix up scorecard header

--- src/pages/constituency/[slug].tsx
+++ src/pages/constituency/[slug].tsx
@@ -1,14 +1,15 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Image from "next/image";
 import kebabCase from "lodash.kebabcase";
-import constituencies from "../data/constituencies.json";
-import parties from "../data/parties.json";
-import { clsx, getPartyColorClass } from "../utils";
 import {
   CheckCircleIcon,
   XCircleIcon,
   QuestionMarkCircleIcon,
 } from "@heroicons/react/24/solid";
+import { clsx } from "../utils";
+
+import constituencies from "../data/constituencies.json";
+import parties from "../data/parties.json";
 
 interface Vote {
   description: string;
@@ -26,85 +27,78 @@ interface MPScorecard {
 }
 
 export const Scorecard: NextPage<{ score: MPScorecard }> = ({ score }) => {
-  const minus =
-    "https://upload.wikimedia.org/wikipedia/commons/3/37/Minus%2C_Web_Fundamentals.svg";
-
-  const mpProfile = (
-    <div className='flex items-center mb-4 p-1'>
-      <Image
-        src={score.thumbnail}
-        width={160}
-        height={160}
-        alt={score.name}
-        className='object-cover object-top w-40 h-40 rounded-full border-black border-2'
-      />
-      <div className='pl-10'>
-        <p className='font-extrabold text-4xl'>{score.name}</p>
-        <p className={`font-bold text-2xl ${score.party.colour}`}>
-          {score.party.name}
-        </p>
-        <p className='font-semibold text-xl'>MP for {score.constituency}</p>
-      </div>
-      <div className='ml-auto mb-auto text-2xl font-bold'>Scorecard</div>
-    </div>
-  );
-
-  const mpVotes = (
-    <div className='space-y-2'>
-      <h2 className='text-2xl font-bold'>
-        During the last Parliament, your MP...
-      </h2>
-      <div className='border-t border-black'></div>
-
-      {score.votes.map((vote, index) => (
-        <div
-          key={index}
-          className='flex flex-row justify-between items-center space-x-2'>
-          <span className='font-medium text-2xl'>{vote.description}</span>
-          {vote.grade == "A" ? (
-            <CheckCircleIcon className='h-16 w-16 text-green-500 shrink-0' />
-          ) : (
-            <XCircleIcon className='h-16 w-16 text-red-500 shrink-0' />
-          )}
-        </div>
-      ))}
-
-      {score.absent.length ? (
-        <>
-          {score.absent.map((vote, index) => (
-            <div
-              key={index}
-              className='flex flex-row justify-between items-center space-x-2'>
-              <span className='text-2xl font-medium'>{vote}</span>
-              <QuestionMarkCircleIcon className='h-16 w-16 text-yellow-500 shrink-0' />
-            </div>
-          ))}
-
-          <div className='mt-4'>
-            <p className='text-md font-light font-gray-200'>
-              The{" "}
-              <span className='text-yellow-700 font-bold'>
-                yellow question mark icon
-              </span>{" "}
-              means no vote was recorded for your MP. An MP may have no vote
-              recorded for several reasons, including abstaining from a vote,
-              illness or carrying out constituency or ministerial business.
-            </p>
-          </div>
-        </>
-      ) : null}
-    </div>
-  );
-
   return (
     <div
       className={clsx(
-        "min-h-screen min-w-full content-center bg-gradient-to-br from-neutral-200 px-6 lg:px-16",
+        "min-h-screen min-w-full content-center bg-gradient-to-br from-neutral-200 p-6 lg:px-16",
         score.party.gradient
       )}>
-      <div className='flex flex-col p-6 lg:p-10 bg-neutral-100 max-w-3xl rounded-md border-2 border-black'>
-        {mpProfile}
-        {mpVotes}
+      <div className='flex flex-col p-4 md:p-6 lg:p-10 bg-neutral-100 max-w-3xl rounded-md border-2 border-black'>
+        {/* Scorecard header */}
+        <div className='flex flex-col md:flex-row items-center pb-4'>
+          <Image
+            src={score.thumbnail}
+            width={160}
+            height={160}
+            alt={score.name}
+            className='object-cover object-top w-40 h-40 rounded-full border-black border-2 shrink-0'
+          />
+          <div className='pt-4 md:pt-0 md:pl-10 w-full text-center md:text-left'>
+            <p className='text-md font-bold uppercase'>Scorecard</p>
+            <p className='font-extrabold text-4xl'>{score.name}</p>
+            <p className={clsx("font-bold text-2xl", score.party.colour)}>
+              {score.party.name}
+            </p>
+            <p className='font-semibold text-xl'>MP for {score.constituency}</p>
+          </div>
+        </div>
+
+        {/* Votes */}
+        <div className='space-y-2'>
+          <h2 className='text-2xl font-bold'>
+            During the last Parliament, your MP voted...
+          </h2>
+          <div className='border-t border-black'></div>
+
+          {score.votes.map((vote, index) => (
+            <div
+              key={index}
+              className='flex flex-row justify-between items-center space-x-2'>
+              <span className='font-medium text-2xl'>{vote.description}</span>
+              {vote.grade == "A" ? (
+                <CheckCircleIcon className='h-16 w-16 text-green-500 shrink-0' />
+              ) : (
+                <XCircleIcon className='h-16 w-16 text-red-500 shrink-0' />
+              )}
+            </div>
+          ))}
+
+          {score.absent.length ? (
+            <>
+              {score.absent.map((vote, index) => (
+                <div
+                  key={index}
+                  className='flex flex-row justify-between items-center space-x-2'>
+                  <span className='text-2xl font-medium'>{vote}</span>
+                  <QuestionMarkCircleIcon className='h-16 w-16 text-yellow-500 shrink-0' />
+                </div>
+              ))}
+
+              <div className='pt-4'>
+                <p className='text-md font-light font-gray-200'>
+                  The{" "}
+                  <span className='text-yellow-700 font-bold'>
+                    yellow question mark icon
+                  </span>{" "}
+                  means no vote was recorded for your MP. An MP may have no vote
+                  recorded for several reasons, including abstaining from a
+                  vote, illness or carrying out constituency or ministerial
+                  business.
+                </p>
+              </div>
+            </>
+          ) : null}
+        </div>
       </div>
     </div>
   );
@@ -161,7 +155,7 @@ export const getStaticProps: GetStaticProps<
             grade: "A",
           },
           {
-            description: "voted for world peace",
+            description: "voted for a ban on fracking",
             grade: "A",
           },
           {
