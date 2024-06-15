5981f7354e62e543213327febea268922e36f7cb
Leo Riviera <leo.riviera@voxa.dev>
2024-06-15 15:27:44 +0100

    add meta tags

--- src/pages/constituency/[slug].tsx
+++ src/pages/constituency/[slug].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import kebabCase from "lodash.kebabcase";
 import {
@@ -30,85 +31,105 @@ type MPScorecard = {
 
 export const Scorecard: NextPage<{ data: MPScorecard }> = ({ data }) => {
   return (
-    <div
-      className={clsx(
-        "min-h-screen min-w-full content-center bg-gradient-to-br from-neutral-200 p-6 lg:px-16",
-        data.party.gradient
-      )}>
-      <div className='flex flex-col max-w-3xl p-4 border-2 border-black rounded-md md:p-6 lg:p-10 bg-neutral-100'>
-        {/* Scorecard header */}
-        <div className='flex flex-col items-center md:flex-row'>
-          <Image
-            src={data.member.thumbnail}
-            width={160}
-            height={160}
-            alt={data.member.name}
-            className='object-cover object-top w-40 h-40 border-2 border-black rounded-full shrink-0'
-          />
-          <div className='w-full pt-4 text-center md:pt-0 md:pl-10 md:text-left'>
-            <p className='text-sm font-bold uppercase md:text-md'>Scorecard</p>
-            <p className='text-2xl font-extrabold md:text-4xl'>
-              {data.member.name}
-            </p>
-            <p
-              className={clsx(
-                "font-bold text-xl md:text-2xl",
-                data.party.colour
-              )}>
-              {data.party.name}
-            </p>
-            <p className='font-semibold md:text-xl'>
-              MP for {data.constituency.name}
-            </p>
-          </div>
-        </div>
-
-        <div className='my-4 -mx-4 border border-black md:-mx-6 lg:-mx-10'></div>
-
-        {/* Votes */}
-        <div className='space-y-2'>
-          <h2 className='text-xl font-bold md:text-2xl'>
-            During the last Parliament, your MP...
-          </h2>
-
-          {data.votes.map((vote, index) => (
-            <div
-              key={index}
-              className='flex flex-row items-center justify-between space-x-2'>
-              <p className='text-xl md:text-2xl'>{vote.description}</p>
-              {
-                {
-                  tick: (
-                    <CheckCircleIcon className='w-12 h-12 text-green-500 md:h-16 md:w-16 shrink-0' />
-                  ),
-                  x: (
-                    <XCircleIcon className='w-12 h-12 text-red-500 md:h-16 md:w-16 shrink-0' />
-                  ),
-                  unknown: (
-                    <QuestionMarkCircleIcon className='w-12 h-12 text-yellow-500 md:h-16 md:w-16 shrink-0' />
-                  ),
-                }[vote.grade]
-              }
-            </div>
-          ))}
-
-          {data.votes.filter((v) => v.grade === "unknown").length > 0 && (
-            <div className='md:pt-2'>
-              <p className='text-sm font-light md:text-md font-gray-200'>
-                The{" "}
-                <span className='font-bold text-yellow-700'>
-                  yellow question mark icon
-                </span>{" "}
-                means no vote was recorded for your MP. An MP may have no vote
-                recorded for several reasons, including if they weren&apos;t an
-                MP at the time, if they abstained from a vote, were ill or were
-                carrying out constituency or ministerial business.
+    <>
+      <Head>
+        <title>MP Scorecard</title>
+        <meta
+          name='description'
+          content={`Use MP Scorecard to find out how ${data.member.name} voted over the last Parliament.`}
+        />
+        <meta property='og:title' content='MP Scorecard' />
+        <meta
+          property='og:description'
+          content={`Use MP Scorecard to find out how ${data.member.name} voted over the last Parliament.`}
+        />
+        <meta
+          property='og:image'
+          content='https://mpscorecard.uk/opengraph-image.jpg'
+        />
+      </Head>
+      <div
+        className={clsx(
+          "min-h-screen min-w-full content-center bg-gradient-to-br from-neutral-200 p-6 lg:px-16",
+          data.party.gradient
+        )}>
+        <div className='flex flex-col max-w-3xl p-4 border-2 border-black rounded-md md:p-6 lg:p-10 bg-neutral-100'>
+          {/* Scorecard header */}
+          <div className='flex flex-col items-center md:flex-row'>
+            <Image
+              src={data.member.thumbnail}
+              width={160}
+              height={160}
+              alt={data.member.name}
+              className='object-cover object-top w-40 h-40 border-2 border-black rounded-full shrink-0'
+            />
+            <div className='w-full pt-4 text-center md:pt-0 md:pl-10 md:text-left'>
+              <p className='text-sm font-bold uppercase md:text-md'>
+                Scorecard
+              </p>
+              <p className='text-2xl font-extrabold md:text-4xl'>
+                {data.member.name}
+              </p>
+              <p
+                className={clsx(
+                  "font-bold text-xl md:text-2xl",
+                  data.party.colour
+                )}>
+                {data.party.name}
+              </p>
+              <p className='font-semibold md:text-xl'>
+                MP for {data.constituency.name}
               </p>
             </div>
-          )}
+          </div>
+
+          <div className='my-4 -mx-4 border border-black md:-mx-6 lg:-mx-10'></div>
+
+          {/* Votes */}
+          <div className='space-y-2'>
+            <h2 className='text-xl font-bold md:text-2xl'>
+              During the last Parliament, your MP...
+            </h2>
+
+            {data.votes.map((vote, index) => (
+              <div
+                key={index}
+                className='flex flex-row items-center justify-between space-x-2'>
+                <p className='text-xl md:text-2xl'>{vote.description}</p>
+                {
+                  {
+                    tick: (
+                      <CheckCircleIcon className='w-12 h-12 text-green-500 md:h-16 md:w-16 shrink-0' />
+                    ),
+                    x: (
+                      <XCircleIcon className='w-12 h-12 text-red-500 md:h-16 md:w-16 shrink-0' />
+                    ),
+                    unknown: (
+                      <QuestionMarkCircleIcon className='w-12 h-12 text-yellow-500 md:h-16 md:w-16 shrink-0' />
+                    ),
+                  }[vote.grade]
+                }
+              </div>
+            ))}
+
+            {data.votes.filter((v) => v.grade === "unknown").length > 0 && (
+              <div className='md:pt-2'>
+                <p className='text-sm font-light md:text-md font-gray-200'>
+                  The{" "}
+                  <span className='font-bold text-yellow-700'>
+                    yellow question mark icon
+                  </span>{" "}
+                  means no vote was recorded for your MP. An MP may have no vote
+                  recorded for several reasons, including if they weren&apos;t
+                  an MP at the time, if they abstained from a vote, were ill or
+                  were carrying out constituency or ministerial business.
+                </p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import kebabCase from "lodash.kebabcase";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { Field, Form, Formik } from "formik";
 import * as yup from "yup";
@@ -13,109 +14,127 @@ export default function Home() {
   const { push } = useRouter();
 
   return (
-    <main
-      className={clsx(
-        "flex-col min-h-screen min-w-full px-6 lg:px-16 content-center bg-gradient-to-br from-yellow-100 via-red-200 to-blue-300",
-        inter.className
-      )}>
-      <div className='mx-auto'>
-        <h1 className='max-w-2xl mb-4 text-3xl font-extrabold text-gray-800 uppercase sm:text-6xl'>
-          How did your MP vote over the last Parliament?
-        </h1>
-        <h2 className='max-w-xl mb-8 text-xl font-bold text-gray-600 uppercase sm:text-4xl'>
-          Enter your postcode to see their scorecard.
-        </h2>
-      </div>
+    <>
+      <Head>
+        <title>MP Scorecard</title>
+        <meta
+          name='description'
+          content='Find out how your MP voted over the last Parliament.'
+        />
+        <meta property='og:title' content='MP Scorecard' />
+        <meta
+          property='og:description'
+          content='Find out how your MP voted over the last Parliament.'
+        />
+        <meta
+          property='og:image'
+          content='https://mpscorecard.uk/opengraph-image.jpg'
+        />
+      </Head>
+      <main
+        className={clsx(
+          "flex-col min-h-screen min-w-full px-6 lg:px-16 content-center bg-gradient-to-br from-yellow-100 via-red-200 to-blue-300",
+          inter.className
+        )}>
+        <div className='mx-auto'>
+          <h1 className='max-w-2xl mb-4 text-3xl font-extrabold text-gray-800 uppercase sm:text-6xl'>
+            How did your MP vote over the last Parliament?
+          </h1>
+          <h2 className='max-w-xl mb-8 text-xl font-bold text-gray-600 uppercase sm:text-4xl'>
+            Enter your postcode to see their scorecard.
+          </h2>
+        </div>
 
-      <div>
-        <Formik
-          initialValues={{
-            postcode: "",
-          }}
-          validationSchema={yup.object().shape({
-            postcode: yup
-              .string()
-              .trim()
-              .required("Please enter a postcode.")
-              .matches(/^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$/i, {
-                message: "That postcode doesn't look valid!",
-              })
-              .notOneOf(
-                ["SANTA1", "SAN TA1"],
-                "Ho ho ho! Santa doesn't have an MP! 🎅🏼"
-              ),
-          })}
-          onSubmit={async (values, { setErrors }) => {
-            const { postcode } = values;
+        <div>
+          <Formik
+            initialValues={{
+              postcode: "",
+            }}
+            validationSchema={yup.object().shape({
+              postcode: yup
+                .string()
+                .trim()
+                .required("Please enter a postcode.")
+                .matches(/^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$/i, {
+                  message: "That postcode doesn't look valid!",
+                })
+                .notOneOf(
+                  ["SANTA1", "SAN TA1"],
+                  "Ho ho ho! Santa doesn't have an MP! 🎅🏼"
+                ),
+            })}
+            onSubmit={async (values, { setErrors }) => {
+              const { postcode } = values;
 
-            const find = `https://api.postcodes.io/postcodes/${encodeURIComponent(
-              postcode
-            )}`;
+              const find = `https://api.postcodes.io/postcodes/${encodeURIComponent(
+                postcode
+              )}`;
 
-            const response = await fetch(find);
+              const response = await fetch(find);
 
-            try {
-              const {
-                result: { parliamentary_constituency },
-              } = (await response.json()) as {
-                result: {
-                  parliamentary_constituency: string;
+              try {
+                const {
+                  result: { parliamentary_constituency },
+                } = (await response.json()) as {
+                  result: {
+                    parliamentary_constituency: string;
+                  };
                 };
-              };
 
-              await push(
-                `/constituency/${kebabCase(parliamentary_constituency)}`
-              );
-            } catch {
-              setErrors({
-                postcode:
-                  "We couldn't find that postcode. Please try another one!",
-              });
-            }
-          }}>
-          {({ touched, errors, submitCount }) => (
-            <Form>
-              <label
-                htmlFor='postcode'
-                className='font-light text-gray-700 uppercase text-md sm:text-2xl'>
-                Your postcode
-              </label>
-              <div className='flex mt-2'>
-                <Field
-                  required
-                  id='postcode'
-                  name='postcode'
-                  placeholder='SW1A 2AA'
-                  className='px-2 border border-gray-300 md:px-4 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-                />
-                <button
-                  type='submit'
-                  className='h-full px-3 py-3 text-sm font-bold text-white bg-red-600 md:text-base md:px-4 rounded-r-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500'>
-                  Submit
-                </button>
-              </div>
-              {touched.postcode && errors.postcode && submitCount >= 1 && (
-                <p className='mt-2 text-sm text-red-600'>{errors.postcode}</p>
-              )}
-            </Form>
-          )}
-        </Formik>
-        <p className='mt-2 font-extralight font-gray-200'>
-          This website uses data from{" "}
-          <a href='https://theyworkforyou.com' className='underline'>
-            TheyWorkForYou
-          </a>
-          ,{" "}
-          <a href='https://developer.parliament.uk' className='underline'>
-            the UK Parliament API
-          </a>{" "}
-          and{" "}
-          <a href='https://postcodes.io' className='underline'>
-            Postcodes.io
-          </a>
-          .
-        </p>
-      </div>
-    </main>
+                await push(
+                  `/constituency/${kebabCase(parliamentary_constituency)}`
+                );
+              } catch {
+                setErrors({
+                  postcode:
+                    "We couldn't find that postcode. Please try another one!",
+                });
+              }
+            }}>
+            {({ touched, errors, submitCount }) => (
+              <Form>
+                <label
+                  htmlFor='postcode'
+                  className='font-light text-gray-700 uppercase text-md sm:text-2xl'>
+                  Your postcode
+                </label>
+                <div className='flex mt-2'>
+                  <Field
+                    required
+                    id='postcode'
+                    name='postcode'
+                    placeholder='SW1A 2AA'
+                    className='px-2 border border-gray-300 md:px-4 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+                  />
+                  <button
+                    type='submit'
+                    className='h-full px-3 py-3 text-sm font-bold text-white bg-red-600 md:text-base md:px-4 rounded-r-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500'>
+                    Submit
+                  </button>
+                </div>
+                {touched.postcode && errors.postcode && submitCount >= 1 && (
+                  <p className='mt-2 text-sm text-red-600'>{errors.postcode}</p>
+                )}
+              </Form>
+            )}
+          </Formik>
+          <p className='mt-2 font-extralight font-gray-200'>
+            This website uses data from{" "}
+            <a href='https://theyworkforyou.com' className='underline'>
+              TheyWorkForYou
+            </a>
+            ,{" "}
+            <a href='https://developer.parliament.uk' className='underline'>
+              the UK Parliament API
+            </a>{" "}
+            and{" "}
+            <a href='https://postcodes.io' className='underline'>
+              Postcodes.io
+            </a>
+            .
+          </p>
+        </div>
+      </main>
+    </>
   );
 }
