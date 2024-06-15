d993b256e85b16293b7c56461c8054e23ef5068f
Leo Riviera <leo.riviera@voxa.dev>
2024-06-15 12:10:59 +0100

    update font sizes on mobile

--- src/pages/constituency/[slug].tsx
+++ src/pages/constituency/[slug].tsx
@@ -13,7 +13,7 @@ import parties from "../data/parties.json";
 
 interface Vote {
   description: string;
-  grade: "A" | "F";
+  grade: "aye" | "noe";
 }
 
 interface MPScorecard {
@@ -35,7 +35,7 @@ export const Scorecard: NextPage<{ score: MPScorecard }> = ({ score }) => {
       )}>
       <div className='flex flex-col p-4 md:p-6 lg:p-10 bg-neutral-100 max-w-3xl rounded-md border-2 border-black'>
         {/* Scorecard header */}
-        <div className='flex flex-col md:flex-row items-center pb-4'>
+        <div className='flex flex-col md:flex-row items-center'>
           <Image
             src={score.thumbnail}
             width={160}
@@ -44,32 +44,46 @@ export const Scorecard: NextPage<{ score: MPScorecard }> = ({ score }) => {
             className='object-cover object-top w-40 h-40 rounded-full border-black border-2 shrink-0'
           />
           <div className='pt-4 md:pt-0 md:pl-10 w-full text-center md:text-left'>
-            <p className='text-md font-bold uppercase'>Scorecard</p>
-            <p className='font-extrabold text-4xl'>{score.name}</p>
-            <p className={clsx("font-bold text-2xl", score.party.colour)}>
+            <p className='text-sm md:text-md font-bold uppercase'>Scorecard</p>
+            <p className='font-extrabold text-2xl md:text-4xl'>{score.name}</p>
+            <p
+              className={clsx(
+                "font-bold text-xl md:text-2xl",
+                score.party.colour
+              )}>
               {score.party.name}
             </p>
-            <p className='font-semibold text-xl'>MP for {score.constituency}</p>
+            <p className='font-semibold md:text-xl'>
+              MP for {score.constituency}
+            </p>
           </div>
         </div>
 
+        <div className='border border-black -mx-4 md:-mx-6 lg:-mx-10 my-4'></div>
+
         {/* Votes */}
         <div className='space-y-2'>
-          <h2 className='text-2xl font-bold'>
-            During the last Parliament, your MP voted...
+          <h2 className='text-xl md:text-2xl font-bold'>
+            During the last Parliament, your MP...
           </h2>
-          <div className='border-t border-black'></div>
 
           {score.votes.map((vote, index) => (
             <div
               key={index}
               className='flex flex-row justify-between items-center space-x-2'>
-              <span className='font-medium text-2xl'>{vote.description}</span>
-              {vote.grade == "A" ? (
-                <CheckCircleIcon className='h-16 w-16 text-green-500 shrink-0' />
-              ) : (
-                <XCircleIcon className='h-16 w-16 text-red-500 shrink-0' />
-              )}
+              <p className='font-medium text-xl md:text-2xl'>
+                {vote.description}
+              </p>
+              {
+                {
+                  aye: (
+                    <CheckCircleIcon className='h-12 w-12 md:h-16 md:w-16 text-green-500 shrink-0' />
+                  ),
+                  noe: (
+                    <XCircleIcon className='h-12 w-12 md:h-16 md:w-16 text-red-500 shrink-0' />
+                  ),
+                }[vote.grade]
+              }
             </div>
           ))}
 
@@ -79,21 +93,23 @@ export const Scorecard: NextPage<{ score: MPScorecard }> = ({ score }) => {
                 <div
                   key={index}
                   className='flex flex-row justify-between items-center space-x-2'>
-                  <span className='text-2xl font-medium'>{vote}</span>
-                  <QuestionMarkCircleIcon className='h-16 w-16 text-yellow-500 shrink-0' />
+                  <span className='text-xl md:text-2xl font-medium'>
+                    {vote}
+                  </span>
+                  <QuestionMarkCircleIcon className='h-12 w-12 md:h-16 md:w-16 text-yellow-500 shrink-0' />
                 </div>
               ))}
 
-              <div className='pt-4'>
-                <p className='text-md font-light font-gray-200'>
+              <div className='md:pt-2'>
+                <p className='text-sm md:text-md font-light font-gray-200'>
                   The{" "}
                   <span className='text-yellow-700 font-bold'>
                     yellow question mark icon
                   </span>{" "}
                   means no vote was recorded for your MP. An MP may have no vote
-                  recorded for several reasons, including abstaining from a
-                  vote, illness or carrying out constituency or ministerial
-                  business.
+                  recorded for several reasons, including if they weren&apos;t
+                  an MP at the time, if they abstained from a vote, were ill or
+                  were carrying out constituency or ministerial business.
                 </p>
               </div>
             </>
@@ -152,16 +168,16 @@ export const getStaticProps: GetStaticProps<
         votes: [
           {
             description: "voted against puppy murder",
-            grade: "A",
+            grade: "aye",
           },
           {
             description: "voted for a ban on fracking",
-            grade: "A",
+            grade: "noe",
           },
           {
             description:
               "voted against cracking down on puppy smuggling and dog thefts",
-            grade: "F",
+            grade: "noe",
           },
         ],
         absent: [
